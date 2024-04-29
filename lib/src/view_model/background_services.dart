b2b51295c01e441954aee06230e03b1efca81a65
Lim seongsu <tjdtn2323@naver.com>
2024-04-29 11:17:50 +0900

    fix: background_services.dart 를 사용하는 예시 수정

--- lib/src/view_model/background_services.dart
+++ lib/src/view_model/background_services.dart
@@ -1,35 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:logger/logger.dart';
 
-class BackgroundServices extends FullLifeCycleController {
-
-
-
-
-  /*App 의 lifeCycle 에 따른 동작
+/*App 의 lifeCycle 에 따른 동작
    ex 1.) background 에서 notification 처리
    ex 2.) foreground 에서 notification 처리 => android 에서 추가로 사용 해야 하는 library local_notification
    ex 3.) dynamicLink 를 lifeCycle 에 따른 처리 필요
    - foreground 에서는 해당 noti 가 생기면 해당 화면으로 전환 등
    - background 에서는 로그인(있을 경우) 자동 로그인 체크 및 invite code 를 통한 App 가입 등
   */
+
+class BackgroundServices extends FullLifeCycleController with WidgetsBindingObserver {
+  //WidgetsBindingObserver : 앱 생명주기 이벤트를 감지하기 위한 인터페이스
+  @override
+  void onInit() {
+    // observer 추가를 통한 didChangeAppLifecycleState 사용
+    WidgetsBinding.instance.addObserver(this);
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.onClose();
+  }
+
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
-    switch(state) {
+    switch (state) {
       case AppLifecycleState.detached:
-        // TODO: Handle this case.
+        Logger().d("detached");
       case AppLifecycleState.resumed:
-        // TODO: Handle this case.
+        Logger().d("resumed");
       case AppLifecycleState.inactive:
-        // TODO: Handle this case.
+        Logger().d("inactive");
       case AppLifecycleState.hidden:
-        // TODO: Handle this case.
+        Logger().d("hidden");
       case AppLifecycleState.paused:
-        // TODO: Handle this case.
+        Logger().d("paused");
     }
     super.didChangeAppLifecycleState(state);
   }
-
-
-
 }
